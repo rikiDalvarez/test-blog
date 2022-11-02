5c08c4cb077cade0ecd27401225b93d4d1eb8f85
Ricardo <ric.dalvarez@hotmail.com>
2022-11-02 10:41:04 +0100

    fix style text color

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,25 +6,25 @@ const categories = [{ name: "React", slug: "React" }, { name: "Web development",
 
 function Header() {
 	return (
-		<div className="continer mx-auto px-10 mb-8">
+		<div className="continer mx-auto px-10 mb-8 text-lime-700">
 			<div className=" border-b w-full inline-block border-blue-400 py-8">
 				<div className="md:floar-left block">
 					<Link href="/">
-						<span className="cursor-pointer font-bold text-4xl text-white">
-							GRAPHCMS
-						</span>
+						<h1 className="cursor-pointer font-bold text-4xl text-Black">
+							Plantio
+						</h1>
 					</Link>
 				</div>
 				<div className="hidden md:float-left md:contents">
 					{categories.map((category) => (
 						<Link key={category.slug} href={`/category/${category.slug}`}>
-							<span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+							<h1 className="md:float-right mt-2 align-middle text-black ml-4 font-semibold cursor-pointer">
 								{category.name}
-							</span>
+							</h1>
 						</Link>
 					))}
 				</div>
-			</div>Header
+			</div>
 		</div>
 	)
 }
