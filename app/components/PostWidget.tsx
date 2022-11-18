f0e659c090e57ec45b25ae2e8c139bbdd30a5a37
Ricardo <ric.dalvarez@hotmail.com>
2022-11-18 14:16:23 +0100

    add to postWidget recent posts

--- app/components/PostWidget.tsx
+++ app/components/PostWidget.tsx
@@ -22,9 +22,20 @@ function PostWidget({ categories, slug }) {
 
 
 	return (
-		<div>
-
-			{relatedPosts.map((post => (<div> {post.title}</div>)))}
+		<div className="bg-orange-100 shadow-lg rounded-lg p-8 mb-8">
+			<h3 className="text-xl mb-8 font-semibold border-b pb-4 border-orange-600">{slug ? "related posts" : "recent posts"}</h3>
+			{relatedPosts.map((post => (
+				<div key={post.title} className="flex items-center w-full mb-4">
+					<div className="w-16 flex-none">
+						<img alt={post.title} height="60px" width="60px" className="align-middle rounded-full" src={post.featuredImage.url} />
+					</div>
+					<div className="flex-grow ml-4">
+						<p className="font-xs">{moment(post.createdAt).format('MMMM DD, YYYY')}</p>
+						<Link href={`/post/${post.slug}`} className="text-md">
+							{post.title}
+						</Link>
+					</div>
+				</div>)))}
 		</div>
 	)
 }
