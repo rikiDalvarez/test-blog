de386688d8e9f713be107db7a002c5b7a1c16763
Ricardo <ric.dalvarez@hotmail.com>
2022-12-05 12:57:53 +0100

    add post folder for pages

---
+++ app/pages/post/[slug].js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { getPosts, getPostsDetails } from "../../services";
+import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm } from "../../components"
+
+const PostDetails = () => {
+	return (
+		<div>PostDetails</div>
+	)
+}
+
+export default PostDetails
