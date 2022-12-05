2339ae8b326b345c6991e9789fcb36cf68542280
Ricardo <ric.dalvarez@hotmail.com>
2022-12-05 14:10:44 +0100

    adding components

--- app/pages/post/[slug].js
+++ app/pages/post/[slug].js
@@ -4,7 +4,23 @@ import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm } fr
 
 const PostDetails = () => {
 	return (
-		<div>PostDetails</div>
+		<div className='container x-auto px-10 mb-8'>
+			<div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+				<div className='col-span-1 lg:col-span-8'>
+					<PostDetail></PostDetail>
+					<Author></Author>
+					<CommentsForm></CommentsForm>
+					<Comments></Comments>
+				</div>
+				<div className='col-span-1 lg:col-span-4'>
+					<div className='relative lg:sticky top-8'>
+						<PostWidget></PostWidget>
+						<Categories></Categories>
+					</div>
+				</div>
+
+			</div>
+		</div>
 	)
 }
 
